773d73d431ba5be992090c5da0cb9ef599934bf4
Bogdan <radumugur1997@gmail.con>
2020-04-04 08:52:59 +0300

    The new style

---
+++ main_frame/main_frame/main_frame_script.js
@@ -0,0 +1,7 @@
+
+function close_filter(){
+    document.getElementById("nav_details_container").style.display="none";
+}
+function show_filter_menu(){
+    document.getElementById("nav_details_container").style.display="flex";
+}
