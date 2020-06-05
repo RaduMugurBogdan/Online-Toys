ae14466be5922e69a636215958ee46c021e9d35f
vTrStefaN <48363523+vTrStefaN@users.noreply.github.com>
2020-06-05 22:44:03 +0300

    Add files via upload

---
+++ View/administrare/components/modal.php
@@ -0,0 +1,8 @@
+<div id="modal">
+    <div id="modal-window">
+        <div id="modal-content">
+            
+        </div>
+        <button id="modal-close" class="lng-btn">Close</button>
+    </div>
+</div>
